48619fb05f2c23e1260927f0ecf3d3a1ed1e9140
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-11-24 22:58:58 +0530

    Decimal to binary file upload

---
+++ src/com/junit/DecimalToBinary.java
@@ -0,0 +1,13 @@
+package com.junit;
+public class DecimalToBinary {
+
+
+        public static void main(String args[]){
+            System.out.println(" Decimal Number to binary conversion  : ");
+        System.out.println(Integer.toBinaryString(10));
+        System.out.println(Integer.toBinaryString(21));
+        System.out.println(Integer.toBinaryString(31));
+    }}
+
+
+
